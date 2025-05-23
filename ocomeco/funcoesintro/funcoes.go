707e11a21b113ec4c7dea3f2cfe6b075470c3863
vitorchx <vitorchx@outlook.com>
2025-05-23 19:01:50 -0300

    Entendendo funções

---
+++ ocomeco/funcoesintro/funcoes.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+// Funçao começanco com letra minúscula:
+// Função é Privada exemplo PrintName = Publica, printName = Privada
+// Função ela só pode ser utilizada no próprio package
+// Como utilizaria ela fora do package: main.PrintName(name)
+
+func main() {
+	//soma(42, 13)
+	fmt.Println(soma(42, 13))
+
+	sub := subtracao(10, 5)
+	fmt.Println(sub)
+
+	name := printName("Vitor")
+	fmt.Println(name)
+}
+
+func subtracao(x int, y int) int {
+	return x - y
+}
+
+func soma(x int, y int) int {
+	return x + y
+}
+
+func printName(name string) string {
+	return name
+}
---
+++ ocomeco/zeroValues/zerovalues.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var i int
+	var f float64
+	var b bool
+	var s string
+
+	fmt.Printf("Inteiro: %v\n", i)
+	fmt.Printf("Float: %v\n", f)
+	fmt.Printf("Boll: %v\n", b)
+	fmt.Printf("Sring: %v\n", s)
+
+}
