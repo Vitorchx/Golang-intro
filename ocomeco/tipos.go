bde75660f3896c03270c0ac6b08d2d2a42373720
vitorchx <vitorchx@outlook.com>
2025-05-23 16:37:41 -0300

    variaveis

---
+++ ocomeco/tipos.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	//Variáveis
+	var name string
+	name = "Vitor"
+	fmt.Println(name)
+
+	name = "steph"
+	fmt.Println(name)
+
+	var idade int
+	idade = 4
+	fmt.Println(idade)
+
+	var a = "steph"
+	fmt.Println(a)
+}
