f1b127d90a5c9ab3691e57c4f8807969fc1c3404
vitorchx <vitorchx@outlook.com>
2025-05-23 17:00:23 -0300

    variaveis e constante introdutório

--- ocomeco/tiporVar/tipos.go
+++ ocomeco/tiporVar/tipos.go
@@ -17,4 +17,20 @@ func main() {
 
 	var a = "steph"
 	fmt.Println(a)
+
+	var b, c int = 1, 2
+	fmt.Println(b)
+	fmt.Println(c)
+
+	var d = true
+	fmt.Println(d)
+
+	//exemplo de inferencia de tipos cooolocando :=
+	f := "apple"
+	fmt.Println(f)
+
+	//Constante
+	const idadeVitor = 4
+	fmt.Println(idadeVitor)
+
 }
